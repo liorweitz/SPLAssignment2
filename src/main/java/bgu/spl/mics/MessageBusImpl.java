41afaa0f614d8d5dc5ca256c9cbfe23b073c01d9
liorweitz <liorweitz@gmail.com>
2020-11-30 15:40:44 +0200

    started messageBus impl

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -7,13 +7,35 @@ import java.util.*;
  * Write your implementation here!
  * Only private fields and methods can be added to this class.
  */
+
+/**
+ * maps:
+ * 1) map between microservice and its queue.
+ * 2) map between event type to the Microservices that subscribed to its type.
+ * 3) map between Broadcast to the Microsevices that subscribed to its type.
+ * 4) map between Event and Future object
+ * 5) map between event Type and counter that measure how many times the sendEvent method
+ * was called for specific type of Event. it is used for the round robin scheduler.
+ *
+ */
 public class MessageBusImpl implements MessageBus {
 
 	private static MessageBusImpl instance=null;
-	private Map<MicroService,Queue> queueMap;
+
+	//Map that stores the events queues of the Microservices, by Microservice
+	private Map<MicroService,Queue<Message>> microToQMap;
+	//Map that connects between an event and an array that holds the microservices that can handle it.
+	private Map<Class<? extends Event>,ArrayList<MicroService>> eventToMicroMap;
+	private Map<Class<? extends Broadcast>,ArrayList<MicroService>> broadcastToMicroMap;
+	private Map<Event,Future> eventToFutureMap;
+	private Map<Class<? extends Event>,Integer> roundRobinMap;
 
 	private MessageBusImpl(){
-		queueMap=new HashMap<>();
+		microToQMap=new HashMap<>();
+		eventToMicroMap=new HashMap<>();
+		broadcastToMicroMap=new HashMap<>();
+		eventToFutureMap=new HashMap<>();
+		roundRobinMap=new HashMap<>();
 	}
 
 	public static MessageBusImpl getInstance(){
@@ -24,44 +46,86 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-
+		if (!eventToMicroMap.containsKey(type)){
+			eventToMicroMap.put(type, new ArrayList<>());
+		}
+		(eventToMicroMap.get(type)).add(m);
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		
+		if (!broadcastToMicroMap.containsKey(type)){
+			broadcastToMicroMap.put(type,new ArrayList<MicroService>());
+		}
+		(broadcastToMicroMap.get(type)).add(m);
     }
 
 	@Override @SuppressWarnings("unchecked")
 	public <T> void complete(Event<T> e, T result) {
-		
+		eventToFutureMap.get(e).resolve(result);
+
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		
+		if (broadcastToMicroMap.containsKey(b.getClass())){
+			for (MicroService m:broadcastToMicroMap.get(b.getClass())){
+				microToQMap.get(m).add(b);
+			}
+		}
 	}
 
-	
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		
-        return null;
+		if (eventToMicroMap.containsKey(e.getClass())) {
+			Future<T> future = new Future<>();
+			eventToFutureMap.put(e, future);
+			Integer robinCounter = roundRobinMap.get(e.getClass());
+			if (robinCounter==null){
+				roundRobinMap.put(e.getClass(),1);
+				robinCounter=1;
+			}
+			ArrayList<MicroService> curr=eventToMicroMap.get(e.getClass());
+			MicroService willHandle=curr.get(robinCounter%curr.size());
+			microToQMap.get(willHandle).add(e);
+			robinCounter++;
+			roundRobinMap.put(e.getClass(),robinCounter);
+			return future;
+		}
+		else{
+			return null;
+		}
 	}
 
+	/**
+	 * not taking care of double registration for the same microservice.
+	 * if re-registration happens a new queue will be allocated.
+	 */
 	@Override
 	public void register(MicroService m) {
-		
+		microToQMap.put(m,new LinkedList<Message>());
 	}
 
 	@Override
 	public void unregister(MicroService m) {
-		
+		if(microToQMap.containsKey(m)){
+			Queue<Message> toBeDeleted=microToQMap.get(m);
+			toBeDeleted.clear();
+			microToQMap.remove(m);
+		}
 	}
 
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
-		
-		return null;
+		if (!microToQMap.containsKey(m)){
+			throw new IllegalStateException("The Microservice is not registered.");
+		}
+		else{
+			try{
+				while(microToQMap.get(m).size()==0)
+					wait();
+			}catch(InterruptedException ex) {throw new InterruptedException();}
+		}
+		return microToQMap.get(m).poll();
 	}
 }
